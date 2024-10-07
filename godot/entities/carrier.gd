685d04fa851860d02763ce17b265ad231bcfd6c3
niposch <niposch@gmail.com>
2024-10-07 17:47:36 +0200

    started reworking item swap

--- godot/entities/carrier.gd
+++ godot/entities/carrier.gd
@@ -14,13 +14,17 @@ func step(x, y, items_future, entities_now, entities_future, level_structure):
 	
 	if items_future[y][x] != null:
 		pickup(items_future[y][x])
+		items_future[y][x].count = 0
 	
 	var other_entity = entities_now[y][next_x]
 	if other_entity == null:
 		other_entity = entities_future[y][next_x]
 	
-	if other_entity != null and $CarriedItem.visible and other_entity.has_method("pickup") and other_entity.pickup($CarriedItem):
-		$CarriedItem.visible = false
+	if other_entity != null  \
+		and direction == 1 \
+		and $CarriedItem.visible \
+		and other_entity.has_method("swap"):
+		other_entity.swap($CarriedItem, self, false)
 	
 	if is_free(next_x, y, entities_now, entities_future) and level_structure[y][next_x] == SimulationState.LEVEL_BLOCK.AIR && level_structure[y+1][next_x] == SimulationState.LEVEL_BLOCK.WALL:
 		move_to(next_x, y, entities_future)
@@ -29,18 +33,28 @@ func step(x, y, items_future, entities_now, entities_future, level_structure):
 		direction *= -1
 		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
 
-func pickup(item) -> bool:
-	# $CarriedItem.add_child(item.get_node("Texture"))
-	# item.queue_free()
-	if $CarriedItem.visible:
-		return false
-	
-	$CarriedItem.visible = true
-	$CarriedItem.item_type = item.item_type
-	$CarriedItem.count = 1
-	
-	item.count -= 1
-	return true
-	#else:
-	#	item.queue_free()
-	
+func pickup(item):
+	if item == null:
+		$CarriedItem.visible = false
+	else:
+		$CarriedItem.visible = true
+		$CarriedItem.item_type = item.item_type
+		$CarriedItem.count = item.count
+
+func swap(item_to_receive, other_swapper, initiated_on_this_entity:bool):
+	if initiated_on_this_entity:
+		var item_to_send = {
+			item_type = $CarriedItem.item_type,
+			count = $CarriedItem.count,
+			visible = $CarriedItem.visible
+		}
+
+		pickup(item_to_receive)
+
+		if item_to_send.visible:
+			return item_to_send
+		else:
+			return null
+	else:
+		var item_received = other_swapper.swap($CarriedItem, self, true)
+		pickup(item_received)
